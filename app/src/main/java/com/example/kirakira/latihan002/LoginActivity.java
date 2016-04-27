08a7f37a290ec70f0b53e306d0ea16ac74bd356e
programerjakarta <programerjakarta@gmail.com>
2016-04-27 15:59:49 +0700

    add font

--- app/src/main/java/com/example/kirakira/latihan002/LoginActivity.java
+++ app/src/main/java/com/example/kirakira/latihan002/LoginActivity.java
@@ -1,23 +1,17 @@
 package com.example.kirakira.latihan002;
 
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         //make windows full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -27,21 +21,9 @@ public class MainActivity extends AppCompatActivity {
         // create folder assets/fonts on app
         //copy font to folder fonts
         TextView tx = (TextView)findViewById(R.id.textView);
-        TextView tz = (TextView)findViewById(R.id.textView2);
 
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Demolition Crack Black.ttf");
 
         tx.setTypeface(custom_font);
-        tz.setTypeface(custom_font);
-
-        //add on click on page
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.mainlayout);
-        rl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
