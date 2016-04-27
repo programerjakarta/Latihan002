e6fba577b3f24fb3934addd7c53b03eb3b3958ae
programerjakarta <programerjakarta@gmail.com>
2016-04-27 14:56:43 +0700

    add font

--- app/src/main/java/com/example/kirakira/latihan002/MainActivity.java
+++ app/src/main/java/com/example/kirakira/latihan002/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.kirakira.latihan002;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        TextView tx = (TextView)findViewById(R.id.textView);
+        TextView tz = (TextView)findViewById(R.id.textView2);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Demolition Crack Black.ttf");
+
+        tx.setTypeface(custom_font);
+        tz.setTypeface(custom_font);
     }
 }
